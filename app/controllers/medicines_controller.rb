72389878e24a2befb7d41ca46632f8901b7637bd
1ain00 <ponpon.krr-n@i.softbank.jp>
2022-09-27 18:28:31 +0900

    カレンダー

--- app/controllers/medicines_controller.rb
+++ app/controllers/medicines_controller.rb
@@ -1,4 +1,14 @@
 class MedicinesController < ApplicationController
   def index
+    @medicines= Medicine.all
+    @medicine = Medicine.new
   end
+
+  
+  private
+
+  def medicine_parameter
+    params.require(:medicine).permit(:name, :content, :start_time)
+  end
+
 end
