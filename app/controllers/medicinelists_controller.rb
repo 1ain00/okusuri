1830401c5d6d6e6009ccd73983aa6da013b6d44a
1ain00 <ponpon.krr-n@i.softbank.jp>
2022-10-07 19:35:24 +0900

    レイアウト作成

--- app/controllers/medicinelists_controller.rb
+++ app/controllers/medicinelists_controller.rb
@@ -1,8 +1,16 @@
 class MedicinelistsController < ApplicationController
   def index
-    @medicinlists = Medicinelist.all
+    @medicinelists = Medicinelist.all
   end
 
+  def new
+    @medicinelist = Medicinelist.new
+  end
+
+  def create
+    @medicinelist = Medicinelist.create(medicinelist_params)
+ 
+  end
 
   def medicinelist_params
     params.require(:medicinelist).permit(:morning, :morningnum, :afternoon, :afternoonnum, :evening, :eveningnum, :beforesleep, :beforesleepnum). merge(user_id: current_user.id)
