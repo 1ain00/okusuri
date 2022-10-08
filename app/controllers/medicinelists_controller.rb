957eea269f27b80c307f58fc40c59075b7c2b501
1ain00 <ponpon.krr-n@i.softbank.jp>
2022-10-08 17:07:36 +0900

    内服タイミング別に投稿＆表示機能実装

--- app/controllers/medicinelists_controller.rb
+++ app/controllers/medicinelists_controller.rb
@@ -3,16 +3,24 @@ class MedicinelistsController < ApplicationController
 
   def new
     @medicinelists = Medicinelist.all
-      @medicinelist = Medicinelist.new
+    @medicinelist = Medicinelist.new
+    @medicinelistmorning = Medicinelist.where(timing_id: "2")
+    @medicinelistafternoon = Medicinelist.where(timing_id: "3")
+    @medicinelistevening = Medicinelist.where(timing_id: "4")
+    @medicinelistbeforesleep = Medicinelist.where(timing_id: "5")
+
   end
 
   def create
-    @medicinelist = Medicinelist.create(medicinelist_params)
+    @medicinelist = Medicinelist.new(medicinelist_params)
+    if @medicinelist.save
+      redirect_to  new_medicinelists_path
+  end
  
   end
 
   def medicinelist_params
-    params.require(:medicinelist).permit(:morning, :morningnum, :afternoon, :afternoonnum, :evening, :eveningnum, :beforesleep, :beforesleepnum)
+    params.require(:medicinelist).permit(:name, :number, :timing_id)
     .merge(user_id: current_user.id)
   end
 end
