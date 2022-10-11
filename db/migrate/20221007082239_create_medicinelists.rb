395fba346338e22d3d2d6c69acb8a22416d74119
1ain00 <ponpon.krr-n@i.softbank.jp>
2022-10-11 12:34:45 +0900

    ER図、画面遷移図作成

--- db/migrate/20221007082239_create_medicinelists.rb
+++ db/migrate/20221007082239_create_medicinelists.rb
@@ -1,10 +1,10 @@
 class CreateMedicinelists < ActiveRecord::Migration[6.0]
   def change
     create_table :medicinelists do |t|
-      t.string :name
-      t.string :number
-      t.integer :user_id
-      t.integer    :timing_id     , null: false
+      t.string :name,null:false
+      t.string :number,null:false
+      t.integer :user_id, null:false
+      t.integer :timing_id, null: false
       t.text :memo
       t.boolean :done, default: false
       t.timestamps
